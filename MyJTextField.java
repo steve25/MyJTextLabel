62fcedcc87603110c6f85ef0d5a90b586388e074
Stefan <stefan.pocsik@gmail.com>
2024-04-11 16:26:28 +0200

    finished

--- MyJTextField.java
+++ MyJTextField.java
@@ -1,13 +1,20 @@
 import javax.swing.*;
+import javax.swing.text.Keymap;
+import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.awt.im.InputContext;
 
 public class MyJTextField extends JTextField {
     public MyJTextField() {
 
+
+
         this.addKeyListener(new KeyListener() {
 
             String oldText;
+            String newChar;
+
 
 
             @Override
@@ -20,41 +27,43 @@ public class MyJTextField extends JTextField {
 
                 switch (e.getKeyCode()) {
                     case 49:
-                        MyJTextField.super.setText(oldText.concat("1").replaceAll("(.)(.)$", "$1"));
+                        newChar = "1";
                         break;
                     case 50:
-                        MyJTextField.super.setText(oldText.concat("2").replaceAll("(.)(.)$", "$1"));
+                        newChar = "2";
                         break;
                     case 51:
-                        MyJTextField.super.setText(oldText.concat("3").replaceAll("(.)(.)$", "$1"));
+                        newChar = "3";
                         break;
                     case 52:
-                        MyJTextField.super.setText(oldText.concat("4").replaceAll("(.)(.)$", "$1"));
+                        newChar = "4";
                         break;
                     case 53:
-                        MyJTextField.super.setText(oldText.concat("5").replaceAll("(.)(.)$", "$1"));
+                        newChar = "5";
                         break;
                     case 54:
-                        MyJTextField.super.setText(oldText.concat("6").replaceAll("(.)(.)$", "$1"));
+                        newChar = "6";
                         break;
                     case 55:
-                        MyJTextField.super.setText(oldText.concat("7").replaceAll("(.)(.)$", "$1"));
+                        newChar = "7";
                         break;
                     case 56:
-                        MyJTextField.super.setText(oldText.concat("8").replaceAll("(.)(.)$", "$1"));
+                        newChar = "8";
                         break;
                     case 57:
-                        MyJTextField.super.setText(oldText.concat("9").replaceAll("(.)(.)$", "$1"));
+                        newChar = "9";
                         break;
                     case 48:
-                        MyJTextField.super.setText(oldText.concat("0").replaceAll("(.)(.)$", "$1"));
+                        newChar = "0";
                         break;
                 }
             }
 
             @Override
             public void keyReleased(KeyEvent e) {
-
+                if (!newChar.isEmpty())
+                    MyJTextField.super.setText(oldText.concat(newChar));
+                newChar = "";
             }
         });
     }
